8a6b258bf6620d5e741bee61c7773795a60e9672
unknown <peretc001@mail.ru>
2019-01-05 14:45:45 +0300

    prt

---
+++ partadami/index.php
@@ -0,0 +1,113 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Купить парты ДЭМИ | Официальный сайт ДЭМИ Краснодар</title> 
+	<meta name="Keywords" content="парта деми, парта деми купить, парты деми, парты деми купить, парта трансформер, парта трансформер купить, официальный дилер деми" /> 
+	<meta name="Description" content="Официальный сайт ДЭМИ по Краснодарскому краю. Описание, цены, отзывы. Доставка. Сборка. Гарантия. Кредит." /> 
+    <?php include $_SERVER['DOCUMENT_ROOT'] .'/inc/header.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] .'/inc/config.php'; ?>
+</head>
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'] .'/inc/nav.php'; ?>
+<div id="partadami">
+	<div class="row">
+		<h1>Детская парта и стул "ДЭМИ"</h1>
+		<img src="/img/cyt17.svg" class="home_img">
+	</div>
+</div>
+<div id="why_dami">
+	<div class="introHolder black">
+		<h2>Зачем вашему ребенку #партадэми</h2>
+	</div>
+	<div class="row">
+		<div class="six columns">
+			<p class="h1"><span>Растет вместе с ребенком</span><br>
+			Парта растет вместе с ребенком с 5-ти лет и до университета (рост от 120 до 198 см)</p>
+		</div>
+		<div class="six columns">
+			<p class="h2"><span>Наклоняется к ребенку</span><br>
+			Изменение угла наклона столешницы позволяет снять нагрузку с позвоночника школьника</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="six columns">
+			<p class="h3"><span>Заботится об осанке</span><br>
+			Парта и стул заботятся о том, чтобы ребенок сидел правильно и не сутулился</p>
+		</div>
+		<div class="six columns">
+			<p class="h4"><span>Стул тоже хорош</span><br>
+			Стул регулируется не только по высоте но и по глубине, снижая нагрузку на позвоночник</p>
+		</div>
+	</div>
+</div>
+<div id="home_img">
+	<div class="introHolder">
+		<h3>Самые популярные товары</h3>
+	</div>	
+	<div class="row">
+		<div class="six columns">
+			<a href="/shop/parta_bez_risunka/cyt14/bez_stula/parta_cyt14-01.php?color=pink"><img src="/img/home/3.jpg"></a>
+		</div>
+		<div class="six columns">
+			<a href="/shop/white/cyt24/bez_stula/parta_cyt24.php?color=white_blue"><img src="/img/home/2.jpg"></a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="six columns">
+			<a href="/shop/parta_bez_risunka/cyt17/bez_stula/parta_cyt17-04-d1.php?color=grey"><img src="/img/home/4.jpg"></a>
+		</div>
+		<div class="six columns">
+			<a href="/shop/white/cyt25/bez_stula/parta_cyt25-03.php?color=white_blue"><img src="/img/home/5.jpg"></a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="twelve columns box desk">
+			<ul>
+				<li><img src="img/desk.svg"></li>
+				<li>скидка на стул «ДЭМИ»<br><small>при покупке с партой</small></li>
+				<li><span class="sale">-25%</span></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div id="video">
+	<video mute autoplay loop>
+		<source src="img/video/1080.mp4" type="video/mp4"></source>
+ 		<source src="img/video/1080.ogv" type="video/ogg"/></source>
+ 		<source src="img/video/1080.webm" type="video/webm"></source>
+	</video>
+</div>
+<div id="home_text">
+	<div class="row">
+		<div class="seven columns">
+			<p>Парты и стулья «ДЭМИ» разработаны с учетом анатомических особенностей растущего организма ребенка и рекомендаций детских врачей, психологов и преподавателей.</p>
+			<p>Лучшее из качеств мебели «ДЭМИ» - правильное развитие опорно-двигательного аппарата ребенка. Парта и стул фирмы «ДЭМИ» созданы, чтобы на позвоночник и мышцы ребенка не ложилась излишняя нагрузка, а зрение не портилось от вынужденно неудобного положения.</p>
+			<p>Парта трансформер и стул «ДЭМИ» предназначены для детей от 5 лет и ростом от 120 до 198 см.</p>
+		</div>
+		<div class="five columns">
+			<img src="/shop/img/element/element.svg">
+		</div>
+	</div>
+	<div class="introHolder">
+		<h2>Парта трансформер и стул «ДЭМИ» растет вместе с ребенком!</h2>
+	</div>
+	<div class="row">
+		<div class="five columns">
+			<img src="/shop/img/element/element2.svg">
+		</div>
+		<div class="seven columns">		
+			<p>Парту и стул «ДЭМИ» (г. Пермь) называют <span>«растущей»</span> мебелью, т.к. она растет вместе с ребенком. Парта трансформер ДЕМИ (г. Пермь) легко подстраивается под рост ребенка. Сам ребенок может изменить угол наклона столешницы. Эргономичный стул регулируется не только по высоте, но и по глубине, чтобы ребенок мог равномерно распределить нагрузку на спину и бедра.</p>
+			<p>Парта Деми прослужит, как минимум, до окончания школы и все это время она будет следить за правильной осанкой Вашего ребенка.</p>
+		</div>
+	</div>
+	<div class="row bottom">
+		<div class="twelve columns">
+			<p>Для того чтобы максимально комфортно организовать рабочее место ребенка мы предлагаем не только купить парты трансформеры дэми, но и в дополнение к ним приобрести тумбы и стеллажи или дополнительные элементы (чехлы на стул, накладки на парты).</p>
+		</div>
+	</div>
+	<div class="row shop">
+		<p><a href="/shop/"><i class="fa fa-bars" aria-hidden="true"></i> Каталог товаров</a></a></p>
+	</div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'] .'/inc/footer.php'; ?>
