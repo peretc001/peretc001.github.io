f4dc4035e2a0149733f2dcf281685034818b2d3b
Игорь Красовский <mac@MacBook-Pro-Igor.local>
2019-04-17 13:53:40 +0300

    wp.autoprkt.ru

--- app/js/script.js
+++ app/js/script.js
@@ -6,23 +6,4 @@ jQuery(document).ready(function ($) {
 		$('.navbar').on('hide.bs.collapse', function () {
 			$('.hamburger').removeClass('is-active');
 		});
-
-	$(window).on('scroll', function () {
-		// remove any existing active
-		$('.active').removeClass("active");
-
-		// change bg color of active section
-		$('.navbar').addClass("active");
-
-		// change bg color of navbar
-		$(".navbar").addClass("active");
-
 	});
-
-	$(window).scroll(function () {
-		/* remove style on scroll back to top */
-		if ($(document).scrollTop() < 60) {
-			$('.navbar').removeClass('active');
-		}
-	});
-});
--- wp.autoprkt.ru/js/script.js
+++
@@ -1,9 +0,0 @@
-
-jQuery(document).ready(function ($) {
-		$('.navbar').on('show.bs.collapse', function () {
-			$('.hamburger').addClass('is-active');
-		});
-		$('.navbar').on('hide.bs.collapse', function () {
-			$('.hamburger').removeClass('is-active');
-		});
-	});
