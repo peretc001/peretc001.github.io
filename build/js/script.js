ca88eb8da4203510d99f3363354ef66c63e14004
Игорь Красовский <mac@MacBook-Pro-Igor.local>
2019-04-28 16:05:54 +0300

    build

---
+++ build/js/script.js
@@ -0,0 +1,22 @@
+//Разрешение экрана
+window.onload = function(){
+    console.log(window.screen.availWidth- (window.screen.availWidth - window.innerWidth));
+};
+$(window).on('scroll', function () {
+    // remove any existing active
+    $('.navbar').removeClass("active");
+
+    // change bg color of active section
+    $('.navbar').addClass("active");
+
+    // change bg color of navbar
+    $(".navbar").addClass("active");
+
+});
+
+$(window).scroll(function () {
+    /* remove style on scroll back to top */
+    if ($(document).scrollTop() < 60) {
+        $('.navbar').removeClass('active');
+    }
+});
