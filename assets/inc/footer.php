8fd3032fc64c7e30b74e4a1c287145937f7db41c
Krasovsky Igor <peretc001@mail.ru>
2019-05-08 00:17:17 +0300

    index

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,15 +1,15 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:400,700,800" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/css/hamburgers.min.css">
-	<link rel="stylesheet" href="/css/animate.css">
-	<link rel="stylesheet" href="/css/main.min.css">
+	<link rel="stylesheet" href="/assets/css/hamburgers.min.css">
+	<link rel="stylesheet" href="/assets/css/animate.css">
+	<link rel="stylesheet" href="/assets/css/main.min.css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	
 
-	<script src="/js/wow/wow.js"></script>
-	<script src="/js/script.js"></script>
+	<script src="/assets/js/wow/wow.js"></script>
+	<script src="/assets/js/script.js"></script>
 	
---
+++ php/app/assets/inc/header.php
@@ -0,0 +1,9 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="icon" href="/assets/img/favicon.ico">
+	<style>
+		body {
+			opacity: 0;
+			overflow-x: hidden;
+		}
+	</style>
