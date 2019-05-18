3b882f4c0e4aa39b8f0c56e800c0f886e65c8061
Krasovsky Igor <peretc001@mail.ru>
2019-05-18 14:41:58 +0300

    app

--- app/js/script.js
+++ app/js/script.js
@@ -9,25 +9,7 @@ $(function() {
 	
 	$('[data-toggle="tooltip"]').tooltip();
 
-	// if ($(window).width() < '992') {
-	// 	var max_col_height = 0; // максимальная высота, первоначально 0
-	// 	$('.card-item').each(function(){ // цикл "для каждой из колонок"
-	// 		if ($(this).height() > max_col_height) { // если высота колонки больше значения максимальной высоты,
-	// 			max_col_height = $(this).height(); // то она сама становится новой максимальной высотой
-	// 		}
-	// 	});
-	// 	var max_gen_col_height = 0;
-	// 	$('.col-sm-6').each(function(){ // цикл "для каждой из колонок"
-	// 		if ($(this).height() > max_gen_col_height) { // если высота колонки больше значения максимальной высоты,
-	// 			max_gen_col_height = $(this).height(); // то она сама становится новой максимальной высотой
-	// 		}
-	// 	});
-	// 	$('.col-sm-6').height(max_col_height);
-	// 	console.log(max_col_height);
-	// 	console.log(max_gen_col_height);
-	// 	Maxheight = max_col_height + max_gen_col_height;
-	// 	console.log(Maxheight);
-	// 	$('.general-card').height(max_gen_col_height);
-		
-	// }
+	$(grid).gridLayout({
+		'resize' : function() {} 
+	})
 });
