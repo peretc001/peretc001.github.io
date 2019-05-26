3598e1963f6cbe85f424e1a24ca52af22519dd81
Krasovsky Igor <peretc001@mail.ru>
2019-05-27 00:16:50 +0300

    react --json-server

--- react/first-react/src/components/Posts.js
+++ react/first-react/src/components/Posts.js
@@ -1,12 +1,73 @@
 import React, {Component} from 'react';
-import Post from './Post';
+import User from './User';
+import InstaService from '../services/instaservices';
+import ErrorMessage from '../components/ErrorMessage';
 
 export default class Posts extends Component {
+   InstaService = new InstaService();
+    
+   state = {
+      posts: [],
+      error: false
+   }
+
+   componentDidMount() {
+      this.updatePosts();
+   }
+
+   updatePosts() {
+      this.InstaService.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError);
+   }
+
+   onPostsLoaded = (posts) => {
+      this.setState({
+         posts,
+         error: false
+      })
+   }
+
+   onError = (err) => {
+      this.setState({
+         error: true
+      })
+   }
+
+   renderItems(arr) {
+      return arr.map((item) => {
+         const {name, altname, photo, src, alt, descr, id} = item;
+         return (
+            <div key={id} className="post">
+               <User 
+                  src={photo}
+                  alt={altname}
+                  name={name}
+                  min/>
+               <img src={src} alt={alt}></img>
+               <div className="post__name">
+                  {name}
+               </div>
+               <div className="post__descr">
+                  {descr}
+               </div>
+            </div>
+         )
+      })
+   }
+
+
    render() {
+      const {error, posts} = this.state;
+      if(error) {
+         return <ErrorMessage/>
+      }
+
+      const items = this.renderItems(posts);
+
       return(
          <div className="left">
-            <Post alt="Природа" src="https://img3.badfon.ru/original/1920x1080/e/78/priroda-derevya-les.jpg"/>
-            <Post alt="Природа" src="https://avatars.mds.yandex.net/get-pdb/226447/f15b2366-d952-4f8a-9b94-de290a2140f0/s1200?webp=false"/>
+            {items}
          </div>
       )
    }
