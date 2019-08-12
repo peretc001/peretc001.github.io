4ed5efaa927bd3266a117920f437ce4bd92de9fd
Krasovsky Igor <peretc001@mail.ru>
2019-08-12 08:02:34 +0300

    BSV

---
+++ dist/templates/BSV/prazdnik-krasnodar/thankyou.php
@@ -0,0 +1,23 @@
+<?php 
+$admin_email = 'i.krasovsky@yandex.ru';
+
+if ($_POST['phone'] != '' and $_POST['human'] == 'human') {
+
+	$to  = $admin_email; 
+	$subject = 'Взрослые праздники: Заявка с сайта'; 
+	$headers .= "Content-type: text/html; charset=utf-8 \r\n"; 
+	$headers .= "From: ". $admin_email ." <". $admin_email .">\r\n";
+	if($_POST['method'] != '') {
+		$message = 'Имя: <b>'. $_POST['name'] .'</b><br>Телефон:  <b>'. $_POST['phone'] .'</b><br>Способ связи: <b>'. $_POST['method'] .'</b><br>';
+	} else {
+		$message = 'Имя: <b>'. $_POST['name'] .'</b><br>Телефон:  <b>'. $_POST['phone'] .'</b><br>';
+	}
+	
+		mail($to, $subject, $message, $headers);
+	?>
+
+<?php } else { ?>
+
+<div class="error">Ошибка</div>
+
+<?php } ?>
