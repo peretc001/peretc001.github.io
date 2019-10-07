aedd9b7b21914eac0057be3ae5b967ab6a62b457
Krasovsky Igor <peretc001@mail.ru>
2019-10-07 19:38:10 +0300

    sb

---
+++ Vue/millarda-chat/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <List :uid="uid"/>
+    <Message />
+  </div>
+</template>
+
+<script>
+import List from './components/List'
+import Message from './components/Message'
+
+const axios = require('axios');
+
+export default {
+  name: 'app',
+  components: {
+    List, Message
+  },
+  data () {
+    return {
+      uid: 1,
+      
+    }
+  },
+  beforeMount () {
+    axios.get('https://millarda.ru/api/v1/get-chat-room?uid=1', {
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(function (response) {
+      console.log(response.data)
+      this.uid = response.data
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ Vue/millarda-chat/src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+      User {{ uid }}
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'List',
+   props: [
+    'uid',
+  ],
+  beforeMount () {
+     console.log(this.uid)
+  }
+}
+</script>
