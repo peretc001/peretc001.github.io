dad42e326d46837034bc686eff41f7acfe93c5be
Игорь Красовский <mac@MacBook-Pro-Igor.local>
2020-01-30 23:15:32 +0300

    dety

--- dety/js/common.js
+++ dety/js/common.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
             modal = document.querySelector('.modal');
 
     const   hideModalCloseBtn = () => {
-                if ( event.target.className == 'close' ) {
+                if ( event.target.dataset.close == 'close' ) {
                     modal.querySelector('.is-active').classList.remove('is-active')
                     setTimeout(() => {
                         modal.querySelector('.fade').classList.remove('fade')
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
             hideModal = () => {
+                document.body.classList.remove('no-scroll')
                 modal.querySelector('.is-active').classList.remove('is-active')
                 setTimeout(() => {
                     modal.querySelector('.fade').classList.remove('fade')
@@ -33,8 +34,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.removeEventListener('click', hideModalCloseBtn())
             }
             showModal = (event) => {
+                document.body.classList.add('no-scroll')
                 event.preventDefault()
-                const current = document.querySelector('[data-modal="'+ event.target.className +'"]')
+                const current = document.querySelector('[data-modal="'+ event.target.dataset.target +'"]')
 
                 modal.classList.add('is-active')
                 current.classList.add('fade')
@@ -46,9 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
     nav.addEventListener('click', (event) => {
         if (    
-            event.target.className == 'nav-top__city__location' || 
-            event.target.className == 'nav-top__right__user' ||
-            event.target.className == 'nav-top__right__login' 
+            event.target.dataset.toggle == 'modal' 
         ) {
             if ( modal.classList.contains('is-active') ) {
                 hideModal()
@@ -59,79 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     modal.addEventListener('click', (event) => event.target.className == 'modal is-active' ? hideModal() : '')
     
-    // const   modalBtn = document.querySelectorAll('[data-toggle="modal"]')
-    //         hideModalNav = (current) => event => {
-    //             if (document.querySelector('.nav').contains(event.target) && modal.classList.contains('is-active')) {
-    //                 hideModal2()
-    //             }
-    //         }
-    //         hideModalCloseBtn = (current) => event => (event.target == current.querySelector('.close') && modal.classList.contains('is-active')) ? hideModal(current) : ''
-    //         hideModalEsc = (current) => event => (event.keyCode == 27 && modal.classList.contains('is-active')) ? hideModal(current) : ''
-    //         showModal = (current) => {
-    //             if ( modal.classList.contains('is-active') ) {
-    //                 hideModal2()
-    //             } else {
-    //             // modal.querySelector('.is-active') ? hideModal(modal.querySelector('.is-active')) : ''
-    //                 modal.classList.add('is-active')
-    //                 current.classList.add('fade')
-    //                 setTimeout(() => {
-    //                     current.classList.add('is-active')
-    //                     document.addEventListener('keydown', hideModalEsc(current))
-    //                     document.addEventListener('click', hideModalCloseBtn(current))
-    //                     document.addEventListener('click', hideModalNav(current))
-    //                 }, 1);
-    //             }
-    //         }
-    //         hideModal = (current) => {
-    //             current.classList.remove('is-active')
-    //             setTimeout(() => {
-    //                 current.classList.remove('fade')
-    //                 modal.classList.remove('is-active')
-    //             }, 200);
-    //             document.removeEventListener('keydown', hideModalEsc)
-    //             document.removeEventListener('click', hideModalCloseBtn)
-    //             document.removeEventListener('click', hideModalNav)
-    //         }
-    //         hideModal2 = () => {
-    //             if ( modal.classList.contains('is-active') ) {
-    //                 modal.querySelector('.is-active').classList.remove('is-active')
-    //                 setTimeout(() => {
-    //                     modal.querySelector('.fade').classList.remove('fade')
-    //                     modal.classList.remove('is-active')
-    //                 }, 200);
-    //                 document.removeEventListener('keydown', hideModalEsc)
-    //                 document.removeEventListener('click', hideModalCloseBtn)
-    //                 document.removeEventListener('click', hideModalNav)
-    //             }
-    //         }
-
-
-    // modalBtn.forEach(item => {
-    //     const current = item.getAttribute('data-target')
-    //     const currentModal = modal.querySelector('.modal-body.' + current)
-    //     item.addEventListener('click', (e) => {
-    //         e.preventDefault()
-    //         if ( modal.classList.contains('.is-active') ) { 
-                
-    //             modal.querySelectorAll('.is-active').forEach(item => {
-    //                 item.classList.remove('is-active')
-    //                 setTimeout(() => {
-    //                     item.classList.remove('fade')
-    //                     modal.classList.remove('is-active')
-    //                 }, 200);
-    //                 showModal(currentModal)
-    //             })
-                
-                
-    //         } else {
-    //             showModal(currentModal)
-    //         }
-    //     })
-    // });
-    // modal.addEventListener('click', (e) => {
-    //     !modal.querySelector('.is-active').contains(e.target) ? hideModal(modal.querySelector('.is-active')) : ''
-    // })
-        
+    
         
 
         //Catalog
