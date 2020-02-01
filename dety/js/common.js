67dc180aaf2455c5d6e7e02dc660501c02a3ec7d
Игорь Красовский <mac@MacBook-Pro-Igor.local>
2020-02-02 00:14:02 +0300

    dety

--- dety/js/common.js
+++ dety/js/common.js
@@ -68,23 +68,84 @@ document.addEventListener("DOMContentLoaded", function() {
             searchCategoryList.forEach(item => item.addEventListener('click', (e) => searchCategory.children[0].textContent = item.textContent) )
 
     //Catalog
-    const   catalogBtn = document.querySelector('.nav-catalog h2')
-            catalogMenu = document.querySelector('.nav-catalog ul')
+    const   catalog = document.querySelector('.nav-catalog')
+            catalogMenu = document.querySelector('.nav-catalog__menu')
+            catalogSubMenu = document.querySelector('.nav-catalog__submenu')
+            catalogSubMenuItem = document.querySelectorAll('.nav-catalog__submenu__item')
 
-    const targetHideCatalog = (e) => (!catalogMenu.contains(e.target) && e.target != catalogMenu) ? hideCatalog() : ''
-    const showCatalog = (e) => {
-        e.preventDefault()
-        catalogMenu.classList.add('active')
-        setTimeout(() => {
-            document.addEventListener('click', targetHideCatalog)
-        }, 100);
-    }
-    const hideCatalog = () => {
-        catalogMenu.classList.remove('active')
-        document.removeEventListener('click', targetHideCatalog)
-    }
+            catalogMenuLink = catalogMenu.querySelectorAll('ul li')
+            catalogMenu.addEventListener('mouseover', () => {
+                catalog.classList.add('in')
+            })
+            catalogMenuLink.forEach(item => {
+                item.addEventListener('mouseover', () => {
+                    item.classList.add('active')
+                    catalog.classList.add('in')
+                    localStorage.setItem('submenu', item.getAttribute('data-menu'))
+                })
+            })
 
-    catalogBtn.addEventListener('click', showCatalog)
+
+            showCatalogSubMenuItem = () => {
+                catalogMenuLink[localStorage.getItem('submenu')].classList.add('active')
+                catalogSubMenuItem[localStorage.getItem('submenu')].classList.add('is-active')
+            }
+            hideCatalogSubMenuItem = () => {
+                catalogMenuLink.forEach((item,i) => {
+                    if ( !localStorage.getItem('submenu') || i != localStorage.getItem('submenu') ) item.classList.remove('active')
+                })
+                catalogSubMenuItem.forEach((item, i) => {
+                    if ( !localStorage.getItem('submenu') || i != localStorage.getItem('submenu') ) {
+                        item.classList.remove('is-active')
+                        // catalogSubMenu.style.WebkitTransition = 'max-height 1s';
+                        // catalogSubMenu.style.MozTransition = 'max-height 1s';
+                        // catalogSubMenu.style.transition = 'max-height 1s';
+                        // item.style.WebkitTransition = 'opacity 1s';
+                        // item.style.MozTransition = 'opacity 1s';
+                        // item.style.transition = 'opacity 1s';
+
+                    }
+                })
+            }
+            
+
+            document.addEventListener('mouseover', () => {
+                if (localStorage.getItem('submenu') && catalog.classList.contains('in')) {
+                    hideCatalogSubMenuItem()
+                    showCatalogSubMenuItem()
+                } else {
+                    hideCatalogSubMenuItem()
+                }
+            })
+
+            document.querySelector('.nav-search').addEventListener('mouseover', () => {
+                localStorage.setItem('submenu', '')
+                hideCatalogSubMenuItem()
+                catalog.classList.remove('in')
+            })
+            document.querySelector('main').addEventListener('mouseover', () => {
+                localStorage.setItem('submenu', '')
+                hideCatalogSubMenuItem()
+                catalog.classList.remove('in')
+            })
+
+
+
+
+    // const targetHideCatalog = (e) => (!catalogMenu.contains(e.target) && e.target != catalogMenu) ? hideCatalog() : ''
+    // const showCatalog = (e) => {
+    //     e.preventDefault()
+    //     catalogMenu.classList.add('active')
+    //     setTimeout(() => {
+    //         document.addEventListener('click', targetHideCatalog)
+    //     }, 100);
+    // }
+    // const hideCatalog = () => {
+    //     catalogMenu.classList.remove('active')
+    //     document.removeEventListener('click', targetHideCatalog)
+    // }
+
+    // catalogBtn.addEventListener('click', showCatalog)
 
     
     
