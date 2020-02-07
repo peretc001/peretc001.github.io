78ea9d57e8023a4867ec1f38c336603d876134f0
Игорь Красовский <mac@MacBook-Pro-Igor.local>
2020-02-07 10:29:46 +0300

    dety fix

--- dety/js/common.js
+++ dety/js/common.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    if (document.querySelector('.nav')) {
     const   nav = document.querySelector('.nav')
             modal = document.querySelector('.modal');
 
@@ -110,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             })
             modal.addEventListener('click', (event) => event.target.className == 'modal in is-active' ? hideModal() : '')
-    
+    }
 
     //Search
     const   searchCategory = document.querySelector('.nav-search__form__category')
@@ -119,79 +120,66 @@ document.addEventListener("DOMContentLoaded", function() {
             searchCategoryList.forEach(item => item.addEventListener('click', (e) => searchCategory.children[0].textContent = item.textContent) )
 
     //Catalog
-    // const   catalog = document.querySelector('.nav-catalog')
-    //         catalogMenu = document.querySelector('.nav-catalog__menu')
-    //         // catalogSubMenu = document.querySelector('.nav-catalog__submenu')
-    //         catalogSubMenuItem = document.querySelectorAll('.nav-catalog__submenu__item')
+    const   catalog = document.querySelector('.nav-catalog')
+            catalogMenu = document.querySelector('.nav-catalog__menu')
+            // catalogSubMenu = document.querySelector('.nav-catalog__submenu')
+            catalogSubMenuItem = document.querySelectorAll('.nav-catalog__submenu__item')
 
-    //         catalogMenuLink = catalogMenu.querySelectorAll('ul li')
-    //         catalogMenu.addEventListener('mouseover', () => {
-    //             catalog.classList.add('in')
-    //         })
-    //         catalogMenuLink.forEach(item => {
-    //             item.addEventListener('mouseover', () => {
-    //                 item.classList.add('active')
-    //                 catalog.classList.add('in')
-    //                 localStorage.setItem('submenu', item.getAttribute('data-menu'))
-    //             })
-    //         })
+            // catalogMenuLink = catalogMenu.querySelectorAll('ul li')
+            // catalogMenu.addEventListener('mouseover', () => {
+            //     catalog.classList.add('in')
+            // })
+            // catalogMenuLink.forEach(item => {
+            //     item.addEventListener('mouseover', () => {
+            //         item.classList.add('active')
+            //         catalog.classList.add('in')
+            //         localStorage.setItem('submenu', item.getAttribute('data-menu'))
+            //     })
+            // })
 
-    //         showCatalogSubMenuItem = () => {
-    //                 catalogMenuLink[localStorage.getItem('submenu')].classList.add('active')
-    //                 catalogSubMenuItem[localStorage.getItem('submenu')].classList.add('is-active')
-    //                 nav.style.WebkitTransition = 'max-height 1s';
-    //                 nav.style.MozTransition = 'max-height 1s';
-    //                 nav.style.transition = 'max-height 1s';
-    //         }
-    //         hideCatalogSubMenuItem = () => {
-    //             catalogMenuLink.forEach((item,i) => {
-    //                 if ( !localStorage.getItem('submenu') || i != localStorage.getItem('submenu') ) item.classList.remove('active')
-    //             })
-    //             catalogSubMenuItem.forEach((item, i) => {
-    //                 if ( !localStorage.getItem('submenu') || i != localStorage.getItem('submenu') ) {
-    //                     item.classList.remove('is-active')
-    //                     nav.style.WebkitTransition = '';
-    //                     nav.style.MozTransition = '';
-    //                     nav.style.transition = '';
-    //                 }
-    //             })
-    //         }
-            
-    //         function sleep(millis) {
-    //             var t = (new Date()).getTime();
-    //             var i = 0;
-    //             while (((new Date()).getTime() - t) < millis) {
-    //                 i++;
-    //             }
-    //         }
-    //         document.addEventListener('mouseover', () => {
-    //             if (localStorage.getItem('submenu') && catalog.classList.contains('in')) {
-    //                 hideCatalogSubMenuItem()
-    //                 showCatalogSubMenuItem()
-    //             } else {
-    //                 hideCatalogSubMenuItem()
-    //             }
-    //         })
+            // showCatalogSubMenuItem = () => {
+            //     catalogMenuLink[localStorage.getItem('submenu')].classList.add('active')
+            //     catalogSubMenuItem[localStorage.getItem('submenu')].classList.add('is-active')
+            // }
+            // hideCatalogSubMenuItem = () => {
+            //     catalogMenuLink.forEach((item,i) => {
+            //         if ( !localStorage.getItem('submenu') || i != localStorage.getItem('submenu') ) item.classList.remove('active')
+            //     })
+            //     catalogSubMenuItem.forEach((item, i) => {
+            //         if ( !localStorage.getItem('submenu') || i != localStorage.getItem('submenu') ) {
+            //             item.classList.remove('is-active')
+            //         }
+            //     })
+            // }
 
-    //         document.querySelector('.nav-search').addEventListener('mouseover', () => {
-    //             localStorage.setItem('submenu', '')
-    //             hideCatalogSubMenuItem()
-    //             catalog.classList.remove('in')
-    //         })
-    //         document.querySelector('main').addEventListener('mouseover', () => {
-    //             localStorage.setItem('submenu', '')
-    //             hideCatalogSubMenuItem()
-    //             catalog.classList.remove('in')
-    //         })
+            // document.addEventListener('mouseover', () => {
+            //     if (localStorage.getItem('submenu') && catalog.classList.contains('in')) {
+            //         hideCatalogSubMenuItem()
+            //         showCatalogSubMenuItem()
+            //     } else {
+            //         hideCatalogSubMenuItem()
+            //     }
+            // })
+
+            // document.querySelector('.nav-search').addEventListener('mouseover', () => {
+            //     localStorage.setItem('submenu', '')
+            //     hideCatalogSubMenuItem()
+            //     catalog.classList.remove('in')
+            // })
+            // document.querySelector('main').addEventListener('mouseover', () => {
+            //     localStorage.setItem('submenu', '')
+            //     hideCatalogSubMenuItem()
+            //     catalog.classList.remove('in')
+            // })
 
 
 
-    // const catalogBtn = document.querySelector('.catalog-btn')
-    // catalogBtn.addEventListener('click', (e) => {
-    //     e.preventDefault()
-    //     catalogBtn.querySelector('.hamburger').classList.toggle('open')
-    //     catalog.classList.toggle('mob')
-    // })
+    const catalogBtn = document.querySelector('.catalog-btn')
+    catalogBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        catalogBtn.querySelector('.hamburger').classList.toggle('open')
+        catalog.classList.toggle('mob')
+    })
 
     
     //Brands page
