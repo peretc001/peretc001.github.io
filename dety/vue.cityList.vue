e152f540d86131d840a803e33d941fac91d7e7bc
Игорь Красовский <mac@MacBook-Pro-Igor.local>
2020-02-14 11:07:47 +0300

    dety fix

---
+++ dety/vue.cityList.vue
@@ -0,0 +1,39 @@
+<script>
+    let cityList = new Vue({
+    el: '.cityList',
+    data: {
+      search: '',
+      cityList: [
+        "Москва", 
+        "Абрамцево", 
+        "Алабино", 
+        "Апрелевка", 
+        "Архангельское", 
+        "Ашитково", 
+        "Байконур", 
+        "Бакшеево", 
+        "Балашиха", 
+        "Барыбино", 
+        "Белоомут", 
+        "Белые Столбы", 
+        "Бородино", 
+        "Бронницы", 
+      ],
+      cities: []
+    },
+    beforeMount() {
+      this.cities = this.cityList
+    },
+    methods:{
+      filterList: function(event){
+        if (this.search.length > 0) {
+          this.cities = this.cityList.filter(item => {
+            return item.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+          })
+        } else {
+          this.cities = this.cityList
+        }
+      }
+    }
+  })
+  </script>
