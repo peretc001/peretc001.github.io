eadf48f6081d7eefdc63e162e0633d773868b1d9
peretc001 <peretc001@mail.ru>
2020-03-02 12:02:09 +0300

    dety modal fix

--- dety/js/common.js
+++ dety/js/common.js
@@ -161,6 +161,7 @@ if (document.querySelector('.modal')) {
             
             modalList.forEach(elem => {
                 elem.addEventListener('click', (event) => {
+                    console.log(123)
                     event.preventDefault()
                     if ( modal.classList.contains('is-active') ) {
                         hideModal()
