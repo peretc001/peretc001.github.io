ed9030c7996077c8c8b966a1981b906e3f8be19f
Игорь Красовский <peretc001@mail.ru>
2020-09-16 00:00:58 +0300

    posezony

--- app/js/script.js
+++ app/js/script.js
@@ -45,45 +45,124 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    $('.thumbnails').slick({
-        vertical: true,
-        infinite: true,
-        slidesToShow: 6,
-        asNavFor: '.gallery',
-        focusOnSelect: true,
-        // arrows: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
+    if (document.querySelector('.thumbnails')) {
+        $('.thumbnails').slick({
+            vertical: true,
+            infinite: true,
+            slidesToShow: 3,
+            asNavFor: '.gallery-column',
+            focusOnSelect: true,
+            arrows: false,
+            dots: false,
+            responsive: [
+                {
+                    breakpoint: 576,
+                    settings: {
+                        asNavFor: false,
+                    }
                 }
-            },
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 5,
-                }
-            }
-        ]
-    });
+            ]
+        });
 
-    $('.gallery').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        asNavFor: '.thumbnails',
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    arrows: false,
-                    dots: true
+        $('.gallery-column').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            draggable: true,
+            asNavFor: '.thumbnails',
+            responsive: [
+                {
+                    breakpoint: 576,
+                    settings: {
+                        asNavFor: false,
+                    }
                 }
-            }
-        ]
-    });
+            ]
+        });
+    }
+
+    if (document.querySelector('.recent-products')) {
+        $('.recent-products').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            draggable: true,
+            arrows: true,
+            dots: false,
+            responsive: [
+                {
+                    breakpoint: 1023,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                }
+            ]
+        });
+    }
+
+    if (document.querySelector('.product-item--size')) {
+        const sizes = document.querySelectorAll('.product-item--size .size-items label')
+        const sizeBlock = document.querySelector('.sizes-hover')
+        sizes.forEach(x => {
+            x.addEventListener('mouseover', () => {
+                sizeBlock.style.display = 'flex'
+                sizeBlock.style.top = (x.offsetTop - 130) + 'px'
+                sizeBlock.style.left = x.offsetLeft + 'px'
+
+                const tableSize = document.querySelector('[data-sizes="' + x.dataset.size + '"]')
+
+                sizeBlock.querySelector('.a-size').textContent = tableSize.children[1].textContent
+                sizeBlock.querySelector('.b-size').textContent = tableSize.children[2].textContent
+                sizeBlock.querySelector('.c-size').textContent = tableSize.children[3].textContent
+            })
+            x.addEventListener('mouseout', () => {
+                sizeBlock.style.display = 'none'
+            })
+        })
+
+
+        const modalSize = document.querySelectorAll('.table-size td:first-child')
+        const modalSizeInfo = document.querySelector('.table-size-info')
+
+        modalSize.forEach(x => {
+            x.addEventListener('click', () => {
+                document.querySelector('.table-size .active')
+                    ? document.querySelector('.table-size .active').classList.remove('active')
+                    : ''
+
+                x.classList.toggle('active')
+
+                const id = x.dataset.size
+
+                const currentSize = document.querySelector('.table-size [data-sizes="' + id + '"]')
+
+                modalSizeInfo.querySelector('.a-size').textContent = currentSize.children[1].textContent
+                modalSizeInfo.querySelector('.b-size').textContent = currentSize.children[2].textContent
+                modalSizeInfo.querySelector('.c-size').textContent = currentSize.children[3].textContent
+
+            })
+        })
+    }
+
+    if ( document.querySelector('.product-item--tabs') ) {
+        const tabs = [...document.querySelector('.product-item--tabs').children]
+        const tabsItem = document.querySelectorAll('.tabs-item')
+
+        tabs.forEach((item,i) => {
+            item.addEventListener('click', () => {
+                tabs.forEach(item => item.classList.contains('active') ? item.classList.remove('active') : '')
+                tabsItem.forEach(item => item.classList.contains('active') ? item.classList.remove('active') : '')
+                item.classList.add('active')
+                tabsItem[i].classList.add('active')
+            })
+        })
+    }
 
     if (document.querySelector('.footer')) {
         const footerList = document.querySelectorAll('h5')
@@ -93,4 +172,114 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         })
     }
-})
+
+    if (document.querySelector('.slider')) {
+        $('.slider').slick({
+            draggable: true,
+            slidesToShow: 1,
+            swipeToSlide: true,
+            infinite: true,
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 3000
+        });
+    }
+
+    if (document.querySelector('.fp-categories__slider')) {
+        $('.fp-categories__slider').slick({
+            draggable: true,
+            slidesToShow: 3,
+            swipeToSlide: true,
+            infinite: true,
+            arrows: true,
+            dots: false,
+            autoplay: false,
+            autoplaySpeed: 3000
+        });
+    }
+
+    let inputs = document.querySelectorAll('.phone_mask');
+
+    Array.prototype.forEach.call(inputs, function(input) {
+        new InputMask({
+            selector: input,
+            layout: input.dataset.mask
+        })
+        input.addEventListener('click', function() {
+            if (this.value == '+7') this.setSelectionRange(2,2);
+        })
+    })
+
+});
+
+function InputMask(options) {
+    this.el = this.getElement(options.selector);
+    this.layout = options.layout || '+7 (9__) ___-__-__';
+    this.maskreg = this.getRegexp();
+    this.setListeners();
+}
+
+InputMask.prototype.getRegexp = function() {
+    var str = this.layout.replace(/_/g, '\\d')
+    str = str.replace(/\(/g, '\\(')
+    str = str.replace(/\)/g, '\\)')
+    str = str.replace(/\+/g, '\\+')
+    str = str.replace(/\s/g, '\\s')
+
+    return str;
+}
+
+InputMask.prototype.mask = function(e) {
+    var _this = e.target,
+        matrix = this.layout,
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = _this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) val = def;
+
+    _this.value = matrix.replace(/./g, function(a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+
+    if (e.type == "blur") {
+        var regexp = new RegExp(this.maskreg);
+        if (!regexp.test(_this.value)) _this.value = "";
+    } else {
+        this.setCursorPosition(_this.value.length, _this);
+    }
+}
+
+InputMask.prototype.setCursorPosition = function(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
+    }
+}
+
+InputMask.prototype.setListeners = function() {
+    this.el.addEventListener("input", this.mask.bind(this), false);
+    this.el.addEventListener("focus", this.mask.bind(this), false);
+    this.el.addEventListener("blur", this.mask.bind(this), false);
+}
+
+InputMask.prototype.getElement = function(selector) {
+    if (selector === undefined) return false;
+    if (this.isElement(selector)) return selector;
+    if (typeof selector == 'string') {
+        var el = document.querySelector(selector);
+        if (this.isElement(el)) return el;
+    }
+    return false
+}
+
+InputMask.prototype.isElement = function(element) {
+    return element instanceof Element || element instanceof HTMLDocument;
+}
+
